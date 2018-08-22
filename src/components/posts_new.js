99fe90c86e90cf50ced216bfff5d4bf3d3bf13b2
meng-Shiun <lin.mengshiun@gmail.com>
2018-08-22 16:38:05 +0200

    feat: Add PostsNew component

---
+++ src/components/posts_new.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Route, Link } from 'react-router-dom'
+
+class PostsNew extends React.Component {
+  render () {
+    return (
+      <div>
+        Post New
+      </div>
+    )
+  }
+}
+
+export default PostsNew
