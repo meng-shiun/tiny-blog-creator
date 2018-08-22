ece3dc2faef2c3ec651e617f3768caf32916a043
meng-Shiun <lin.mengshiun@gmail.com>
2018-08-22 12:13:12 +0200

    feat: Set up homepage Route

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
+import { BrowserRouter, Route } from 'react-router-dom'
+import ReduxPromise from 'redux-promise'
 
 import reducers from './reducers'
 import PostsIndex from './components/posts_index'
 
-const createStoreWithMiddleware = applyMiddleware()(createStore)
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore)
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <div>
-      <PostsIndex />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Route exact path="/" component={PostsIndex} />
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'))
