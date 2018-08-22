0e2f6b406d7b8811ea52693e5ffcf5dd77be03eb
meng-Shiun <lin.mengshiun@gmail.com>
2018-08-22 14:23:20 +0200

    feat: Create & Implement FETCH_POSTS Action and Reducer

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,12 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { fetchPosts } from '../actions'
 
 class PostsIndex extends React.Component {
+  componentDidMount() {
+    this.props.fetchPosts()
+  }
+
   render () {
     return (
       <div>Post Index</div>
@@ -8,4 +14,4 @@ class PostsIndex extends React.Component {
   }
 }
 
-export default PostsIndex
+export default connect(null, { fetchPosts })(PostsIndex)
